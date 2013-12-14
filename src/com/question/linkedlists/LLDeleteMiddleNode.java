e387f1493bcc01f7641c0bfde5cd338ecdb0cb4f
Yanna Wu <wuyanna2009@gmail.com>
2013-12-13 22:26:10 -0500

    question 2.4

--- src/com/question/linkedlists/LLDeleteMiddleNode.java
+++ src/com/question/linkedlists/LLDeleteMiddleNode.java
@@ -1,3 +1,17 @@
+/* 
+ * Cracking the coding interview (5th edition)
+ * Chapter 2 - Linked Lists
+ * 
+ * Question 2.3
+ * Implement an algorithm to delete a node in the middle of a singly linked list,
+ * given only access to that node.
+ * EXAMPLE
+ * Input: the node c from the linked list a->b->c->d->e
+ * Result: nothing is returned, but the new linked list looks like a->b->d->e
+ * 
+ * Solution - pg 187
+ * 
+ */
 package com.question.linkedlists;
 
 import com.ds.linkedlist.SinglyLinkedList;
