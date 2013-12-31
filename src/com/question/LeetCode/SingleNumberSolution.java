8ae74469e6656752ce428d50e3e561f5e611acb3
Yanna Wu <wuyanna2009@gmail.com>
2013-12-30 23:58:53 -0500

    merge from qiki

--- src/com/question/LeetCode/SingleNumberSolution.java
+++ src/com/question/LeetCode/SingleNumberSolution.java
@@ -1,17 +1,20 @@
-package com.question.LeetCode;
-/*Question:
- * Given an array of integers, every element appears twice except for one. Find that single one.
- * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+/*
+ * Question:
+ * Given an array of integers, every element appears twice except for one. 
+ * Find that single one.
+ * Your algorithm should have a linear runtime complexity. 
+ * Could you implement it without using extra memory?
+ * 
  */
+package com.question.LeetCode;
+
 public class SingleNumberSolution {
 	public static void main(String[] args) {
 		int[] A = { 2, 4, 2, 5, 4, 6, 5 };
 		SingleNumberSolution s = new SingleNumberSolution();
 		System.out.print(s.singleNumber(A));
 	}
- /* since the 1^1^2^1 = 2
-  * O(n) time complexity without extra space
-  */
+
 	public int singleNumber(int[] A) {
 		int x = 0;
 		for (int i = 0; i < A.length; i++) {
@@ -20,4 +23,3 @@ public class SingleNumberSolution {
 		return x;
 	}
 }
-
