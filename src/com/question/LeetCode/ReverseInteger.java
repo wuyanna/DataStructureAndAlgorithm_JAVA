f3114fe29a6b48ee8ec3b2c436ec78bcdaaaa70f
Yanna Wu <wuyanna2009@gmail.com>
2014-01-02 18:05:58 -0500

    merge leetcode

--- src/com/question/LeetCode/ReverseInteger.java
+++ src/com/question/LeetCode/ReverseInteger.java
@@ -3,27 +3,33 @@
  * Example1: x = 123, return 321
  * Example2: x = -123, return -321
  */
-
 package com.question.LeetCode;
 
 public class ReverseInteger {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int number = -123;
 		ReverseInteger integer = new ReverseInteger();
-		System.out.print(integer.reverse(number));
+		System.out.println(integer.reverse(number));
+		System.out.println(integer.reverseR(number,0));
 	}
 
 	public int reverse(int x) {
 		int result = 0;
-		int num;
-		// 12
-		while (x != 0) {
-			num = x % 10;// 2
-			x = x / 10;// 1
-			result = result * 10 + num;
+		while(x != 0) {
+			result *= 10;
+			result += (x % 10);
+			x /= 10;
 		}
 		return result;
+	}
+
+	// recursive
+	public int reverseR(int x, int result) {
+		if (x == 0)
+			return result;
+		int a = x % 10;
+		int b = x / 10;
+		return reverseR(b, a + result * 10);
 	}
 }
