fc340602b2611150f2ad03c8f3f83d1c641a9936
Yanna Wu <wuyanna2009@gmail.com>
2014-01-03 13:16:37 -0500

    merge leetcode stock

--- src/com/question/LeetCode/stockProfitII.java
+++ src/com/question/LeetCode/stockProfitII.java
@@ -1,27 +1,34 @@
 /*
- * Say you have an array for which the ith element is the price of a given stock on day i.
- * Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+ * Say you have an array for which the ith element is the price of 
+ * a given stock on day i.
+ * 
+ * Design an algorithm to find the maximum profit. You may complete 
+ * as many transactions as you like (ie, buy one and sell one share 
+ * of the stock multiple times). However, you may not engage in 
+ * multiple transactions at the same time (ie, you must sell the 
+ * stock before you buy again).
+ * 
  */
-//o(N)
 package com.question.LeetCode;
 
-public class stockProfitII {
+public class StockProfitII {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int [] prices = {2,3,4,0,5};
-		stockProfitII stock = new stockProfitII();
+		int[] prices = { 2, 3, 4, 0, 5 };
+		StockProfitII stock = new StockProfitII();
 		System.out.print(stock.maxProfit(prices));
 	}
+
+	// o(N)
 	public int maxProfit(int[] prices) {
-        int price = 0;
-        for (int i = 1; i < prices.length ; i++) {
-        	int difference = prices[i] - prices[i-1];
-        
-            if (difference > 0 ) {
-                price = price + difference;
-            }
-        }
-        return price;
-    }
+		int price = 0;
+		for (int i = 1; i < prices.length; i++) {
+			int difference = prices[i] - prices[i - 1];
+
+			if (difference > 0) {
+				price = price + difference;
+			}
+		}
+		return price;
+	}
 }
