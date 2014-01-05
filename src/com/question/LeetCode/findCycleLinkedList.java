ccfb435f6bff239c4f4780c28c60438e596fb7bc
Qiki <qi@brandingbrand.com>
2014-01-05 16:49:50 -0500

    changed style

--- src/com/question/LeetCode/findCycleLinkedList.java
+++ src/com/question/LeetCode/findCycleLinkedList.java
@@ -1,4 +1,5 @@
 package com.question.LeetCode;
+
 /*
  * Given a linked list, determine if it has a cycle in it.
  * Follow up:
@@ -12,16 +13,16 @@ public class findCycleLinkedList {
 		ListNode list = new ListNode(2);
 		ListNode next = new ListNode(3);
 		ListNode prev = new ListNode(3);
-		
+
 		prev.setNext(next);
 		next.setNext(prev);
 		list.setNext(next);
-		
+
 		findCycleLinkedList linkedList = new findCycleLinkedList();
-		
+
 		System.out.print(linkedList.hasCycle(list));
 	}
-	
+
 	public static class ListNode {
 		int val;
 		ListNode next;
@@ -30,42 +31,43 @@ public class findCycleLinkedList {
 			val = x;
 			next = null;
 		}
-		
+
 		public void setNext(ListNode next) {
 			this.next = next;
 		}
+
 		public String toString() {
-		    String result = val + " ";
-		    if (next != null) {
-		        result += next.toString();
-		    }
-		    return result;
+			String result = val + " ";
+			if (next != null) {
+				result += next.toString();
+			}
+			return result;
 		}
 	}
-	
+
 	public boolean hasCycle(ListNode head) {
-        ListNode slow = head;
-        ListNode runner = head;
-        
-        if (head == null) {
-            return false;
-        }
-        
-        while (true) {
-          slow = slow.next;
-          
-          if (runner.next != null) {
-              runner = runner.next.next;
-          } else {
-              return false;
-          }
-          
-          if (slow == null || runner == null) {
-              return false;
-          }
-          if (slow == runner) {
-              return true;
-          }
-        }
-    }
+		ListNode slow = head;
+		ListNode runner = head;
+
+		if (head == null) {
+			return false;
+		}
+
+		while (true) {
+			slow = slow.next;
+
+			if (runner.next != null) {
+				runner = runner.next.next;
+			} else {
+				return false;
+			}
+
+			if (slow == null || runner == null) {
+				return false;
+			}
+			if (slow == runner) {
+				return true;
+			}
+		}
+	}
 }
