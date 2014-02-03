d82a60085feda4bd1a7ac29dd4f08205ed7755ae
Yanna Wu <wuyanna2009@gmail.com>
2014-02-03 16:14:52 -0500

    add build.xml

---
+++ src/SampleTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class SampleTest {
+
+	@Test
+	public void test() {
+//		fail("Not yet implemented");
+		assertTrue(true);
+	}
+
+}
